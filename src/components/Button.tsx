62d4f74e21951172b66b23c23db1a0f00a64fd96
rogersanctus <rogersanctus@gmail.com>
2023-03-31 01:52:52 -0300

    feat: creates a button component with variants, sizes and other stylings

---
+++ src/components/Button.tsx
@@ -0,0 +1,42 @@
+import { ButtonHTMLAttributes } from 'react'
+import './button.css'
+
+export const Variants = [
+  'primary',
+  'secondary',
+  'success',
+  'info',
+  'warning',
+  'error',
+  'dark'
+] as const
+export type Variant = (typeof Variants)[number]
+
+export const Sizes = ['xs', 'sm', 'md', 'lg', 'xl'] as const
+
+export type Size = (typeof Sizes)[number]
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: Variant
+  squared?: boolean
+  size?: Size
+}
+
+export function Button({
+  children,
+  variant = 'primary',
+  squared = false,
+  size = 'md',
+  ...props
+}: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={`${props.className} button button-${variant} button-${size} ${
+        squared ? 'rounded-none' : 'rounded-md'
+      } px-4 py-2 font-semibold`}
+    >
+      {children}{' '}
+    </button>
+  )
+}
