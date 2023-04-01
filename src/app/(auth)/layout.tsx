b294081a46569de40f839fd80ed096ecd14bfe9f
rogersanctus <rogersanctus@gmail.com>
2023-04-01 15:49:13 -0300

    feat: creates the authentication root layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+export const metadata = {
+  title: 'Vai! Compra - Login'
+}
+
+interface AuthLayoutProps {
+  children: React.ReactNode
+}
+
+export default function RootLayout({ children }: AuthLayoutProps) {
+  return (
+    <div className="flex flex-col h-screen">
+      <Header isAuthPage />
+      <main className="h-full">{children}</main>
+      <Footer isAuthPage />
+    </div>
+  )
+}
