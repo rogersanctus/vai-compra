6c923ce74a28341a7fc224e0c3be16dc995038f3
rogersanctus <rogersanctus@gmail.com>
2023-04-01 15:42:32 -0300

    feat: adds an accent mode for the Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,20 @@
 import { InputHTMLAttributes } from 'react'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isAccent?: boolean
+}
 
-export function Input({ ...props }: InputProps) {
+export function Input({ isAccent = false, ...props }: InputProps) {
   return (
     <input
       {...props}
-      className={`${props.className} rounded-md px-5 py-2 text-gray-700 border border-orange-600 focus:outline focus:outline-[3px] focus:outline-orange-500/90`}
+      className={`${
+        props.className
+      } outline-none rounded-md px-5 py-2 text-gray-700 border ${
+        isAccent ? 'border-orange-600' : 'border-neutral-400'
+      } focus:ring ${
+        isAccent ? 'focus:ring-orange-500/90' : 'focus:ring-sky-500'
+      }`}
     />
   )
 }
