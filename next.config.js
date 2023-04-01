dada665552f049b256277b905247c85e38ed53ac
rogersanctus <rogersanctus@gmail.com>
2023-04-01 16:00:27 -0300

    chore: loads the private and public keys files as strings and adds them into the environment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
+const fs = require('fs')
+
+let privKey = undefined
+let pubKey = undefined
+
+try {
+  privKey = fs.readFileSync('priv.key', 'utf8')
+  pubKey = fs.readFileSync('pub.key', 'utf-8')
+} catch (error) {
+  console.error(error)
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true
+  },
+  env: {
+    JWT_PRIV_KEY: privKey,
+    JWT_PUB_KEY: pubKey
   }
 }
 
