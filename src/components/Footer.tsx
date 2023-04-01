efe9234dcc9f0bd9cb125094e82786f56015a05c
rogersanctus <rogersanctus@gmail.com>
2023-04-01 15:44:22 -0300

    feat: adds an auth page mode to Footer and Header components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,67 +6,75 @@ import { Instagram } from './svgs/Instagram'
 import { Linkedin } from './svgs/Linkedin'
 import { Twitter } from './svgs/Twitter'
 
-export function Footer() {
+interface FooterProps {
+  isAuthPage?: boolean
+}
+
+export function Footer({ isAuthPage = false }: FooterProps) {
   return (
     <footer className="bg-blue-800 p-0">
-      <div className="flex bg-amber-500 px-10 py-4 items-center border-y border-amber-600 shadow">
-        <div className="">
-          <span className="ml-auto text-white text-lg font-bold">
-            Fique por dentro de nossas Ofertas
-          </span>
+      {isAuthPage ? null : (
+        <div className="flex bg-amber-500 px-10 py-4 items-center border-y border-amber-600 shadow">
+          <div className="">
+            <span className="ml-auto text-white text-lg font-bold">
+              Fique por dentro de nossas Ofertas
+            </span>
+          </div>
+          <form className="flex flex-grow ml-6">
+            <Input
+              type="text"
+              name="name"
+              placeholder="Qual seu nome?"
+              className="flex-grow"
+              required
+            />
+            <Input
+              type="email"
+              name="email"
+              placeholder="E o seu e-mail?"
+              className="ml-4 flex-grow"
+              required
+            />
+            <Button type="submit" className="ml-4 px-4 py-2" variant="success">
+              Acompanhar
+            </Button>
+          </form>
         </div>
-        <form className="flex flex-grow ml-6">
-          <Input
-            type="text"
-            name="name"
-            placeholder="Qual seu nome?"
-            className="flex-grow"
-            required
-          />
-          <Input
-            type="email"
-            name="email"
-            placeholder="E o seu e-mail?"
-            className="ml-4 flex-grow"
-            required
-          />
-          <Button type="submit" className="ml-4 px-4 py-2" variant="success">
-            Acompanhar
-          </Button>
-        </form>
-      </div>
+      )}
       <div className="relative px-20 py-8 pb-20">
         <div className="absolute fill-current left-0 top-0 bottom-0 right-0">
           <div className="bg-repeated w-full h-full" />
         </div>
         <div className="relative flex gap-6 justify-around">
-          <div>
-            <span className="text-xl uppercase text-white font-bold drop-shadow">
-              Categorias
-            </span>
-            <ul className="text-sky-300 font-semibold mt-4">
-              <li>
-                <a href="#" title="" className="hover:text-sky-100">
-                  Categoria A
-                </a>
-              </li>
-              <li>
-                <a href="#" title="" className="hover:text-sky-100">
-                  Categoria K
-                </a>
-              </li>
-              <li>
-                <a href="#" title="" className="hover:text-sky-100">
-                  Categoria X
-                </a>
-              </li>
-              <li>
-                <a href="#" title="" className="hover:text-sky-100">
-                  Categoria E
-                </a>
-              </li>
-            </ul>
-          </div>
+          {isAuthPage ? null : (
+            <div>
+              <span className="text-xl uppercase text-white font-bold drop-shadow">
+                Categorias
+              </span>
+              <ul className="text-sky-300 font-semibold mt-4">
+                <li>
+                  <a href="#" title="" className="hover:text-sky-100">
+                    Categoria A
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="" className="hover:text-sky-100">
+                    Categoria K
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="" className="hover:text-sky-100">
+                    Categoria X
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="" className="hover:text-sky-100">
+                    Categoria E
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
           <div>
             <span className="text-xl uppercase text-white font-bold drop-shadow">
               Ajuda
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,10 @@ import { AuthInteraction } from './AuthInteraction'
 interface HeaderProps {
   prepend?: React.ReactNode
   append?: React.ReactNode
+  isAuthPage?: boolean
 }
 
-export function Header({ prepend, append }: HeaderProps) {
+export function Header({ prepend, append, isAuthPage = false }: HeaderProps) {
   return (
     <header className="relative flex items-center w-full bg-gradient-to-r from-indigo-700 to-blue-500 px-10 py-8 border-b-[2px] border-blue-300">
       <a href="/" title="Vai Compra" className="flex ml-10 mr-20 items-center">
@@ -23,7 +24,7 @@ export function Header({ prepend, append }: HeaderProps) {
       </a>
       {prepend}
       <div className="flex ml-auto">
-        <AuthInteraction />
+        {isAuthPage ? null : <AuthInteraction />}
         {append}
       </div>
       <div className="absolute bottom-0 left-0 w-full h-[2px] bg-purple-800"></div>
