accfb47d21f2cecc67301018b56cfab3736a401f
rogersanctus <rogersanctus@gmail.com>
2023-04-01 15:51:24 -0300

    feat: creates a session options object to keep the Cookies options setup

---
+++ src/lib/session.ts
@@ -0,0 +1,11 @@
+interface CookieOption {
+  secure: boolean
+  sameSite: 'strict' | 'lax' | 'none'
+  httpOnly: boolean
+}
+
+export const sessionOptions: CookieOption = {
+  secure: process.env.NODE_ENV === 'production',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  httpOnly: true
+}
