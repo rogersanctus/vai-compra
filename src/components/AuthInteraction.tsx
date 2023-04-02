a73e1fe40816d567a7b22018f8ac8788cf7ae6b1
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 03:45:06 -0300

    feat: adds a kind of loading skeleton for the user info

--- src/components/AuthInteraction.tsx
+++ src/components/AuthInteraction.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { UserCircleIcon } from '@heroicons/react/20/solid'
 import { useAppDispatch, useAppSelector } from '@/stores'
 import { actions } from '@/stores/actions'
@@ -41,29 +41,22 @@ export function AuthInteraction() {
     }
   }, [auth.needToFetchUser, dispatch])
 
-  async function onLogout() {
-    try {
-      await fetch('/api/auth/login', { method: 'DELETE' })
+  const UserInfo = useCallback(() => {
+    async function onLogout() {
+      try {
+        await fetch('/api/auth/login', { method: 'DELETE' })
 
-      reset()
-      router.push('/')
-    } catch (error) {
-      console.error(error)
+        reset()
+        router.push('/')
+      } catch (error) {
+        console.error(error)
+      }
     }
-  }
 
-  return (
-    <div
-      title={auth.isLoggedIn ? '' : 'Fazer login ou cadastrar'}
-      className="flex flex-col items-center drop-shadow"
-    >
-      <UserCircleIcon
-        className="text-white w-8 h-8"
-        title={auth.isLoggedIn ? auth.user?.name : ''}
-      />
-      {auth.isLoggedIn ? (
+    if (auth.isLoggedIn) {
+      return (
         <>
-          <span className="text-white">{auth.user?.email}</span>
+          <span className="text-white text-sm">{auth.user?.email}</span>
           <div className="whitespace-nowrap text-purple-100 font-semibold text-xs">
             <button
               onClick={onLogout}
@@ -74,16 +67,35 @@ export function AuthInteraction() {
             </button>
           </div>
         </>
+      )
+    }
+
+    return (
+      <div className="whitespace-nowrap text-purple-100 font-semibold text-xs">
+        <a href="/login" title="Fazer Login" className="mr-1">
+          Login
+        </a>
+        /
+        <a href="/signup" title="Cadastrar" className="ml-1">
+          Cadastrar
+        </a>
+      </div>
+    )
+  }, [auth.isLoggedIn, auth.user, router])
+
+  return (
+    <div
+      title={auth.isLoggedIn ? '' : 'Fazer login ou cadastrar'}
+      className="flex flex-col items-center drop-shadow"
+    >
+      <UserCircleIcon
+        className="text-white w-8 h-8"
+        title={auth.isLoggedIn ? auth.user?.name : ''}
+      />
+      {auth.isLoading ? (
+        <div className="mt-1 bg-gray-300/80 rounded w-24 h-4"></div>
       ) : (
-        <div className="whitespace-nowrap text-purple-100 font-semibold text-xs">
-          <a href="/login" title="Fazer Login" className="mr-1">
-            Login
-          </a>
-          /
-          <a href="/signup" title="Cadastrar" className="ml-1">
-            Cadastrar
-          </a>
-        </div>
+        <UserInfo />
       )}
     </div>
   )
