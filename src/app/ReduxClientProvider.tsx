20a6cb9ed05785fde7565e7c35f37943e78d14ff
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 15:31:46 -0300

    feat: adds the toastify container to the first cliend side component on the DOM

--- src/app/ReduxClientProvider.tsx
+++ src/app/ReduxClientProvider.tsx
@@ -2,11 +2,17 @@
 
 import { Provider } from 'react-redux'
 import { store } from '@/stores'
+import { ToastContainer } from 'react-toastify'
 
 interface ReduxClientProviderProps {
   children: React.ReactNode
 }
 
 export function ReduxClientProvider({ children }: ReduxClientProviderProps) {
-  return <Provider store={store}>{children}</Provider>
+  return (
+    <Provider store={store}>
+      {children}
+      <ToastContainer />
+    </Provider>
+  )
 }
