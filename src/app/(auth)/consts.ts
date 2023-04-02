a2838423662038072599db53c5388aad8b090883
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 15:55:03 -0300

    feat: makes min password length const load its value from env or assume a default value feat: adds a Set of login error messages for given reasons

--- src/app/(auth)/consts.ts
+++ src/app/(auth)/consts.ts
@@ -1,1 +1,10 @@
-export const MIN_PASSWORD_LENGTH = 4
+const envMinPasswordLength = process.env.NEXT_PUBLIC_MIN_PASSWORD_LENGTH
+
+export const MIN_PASSWORD_LENGTH = envMinPasswordLength
+  ? Number(envMinPasswordLength)
+  : 4
+
+export const LoginErrorReasons = {
+  user_not_found: 'Usuário não encontrado',
+  wrong_password: 'Senha inválida'
+}
