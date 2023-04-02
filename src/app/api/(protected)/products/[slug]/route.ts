e35d5bc16a50361335e44ab5c4a166343d8137db
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 17:55:54 -0300

    feat: creates the route to get a product by its id

---
+++ src/app/api/(protected)/products/[slug]/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+import { GetServerSidePropsContext } from 'next'
+import { fetchOnApi } from '@/app/api/externalApi'
+
+export async function GET(
+  request: NextRequest,
+  { params }: GetServerSidePropsContext<{ slug: string }>
+) {
+  const productId = params?.slug
+
+  if (!productId) {
+    return NextResponse.json(
+      { error: { message: 'missing_product_id' } },
+      { status: 404 }
+    )
+  }
+
+  try {
+    const response = await fetchOnApi(`/products/${productId}`)
+
+    if (!response.ok) {
+      throw new Error('Could not fetch the Products from the api')
+    }
+
+    const products = await response.json()
+
+    return NextResponse.json(products)
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(error, { status: 400 })
+    }
+
+    return new NextResponse('Unknown error: ' + JSON.stringify(error), {
+      status: 500
+    })
+  }
+}
