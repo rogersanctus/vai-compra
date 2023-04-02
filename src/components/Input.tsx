54e36518fdc64841e4b1c12027f2f39086ac741e
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 15:29:33 -0300

    feat: makes improvements on the Input component to have an error style and to forward refs to the native input element

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,20 +1,39 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, forwardRef } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isAccent?: boolean
+  hasError?: boolean
 }
 
-export function Input({ isAccent = false, ...props }: InputProps) {
-  return (
-    <input
-      {...props}
-      className={`${
-        props.className
-      } outline-none rounded-md px-5 py-2 text-gray-700 border ${
-        isAccent ? 'border-orange-600' : 'border-neutral-400'
-      } focus:ring ${
-        isAccent ? 'focus:ring-orange-500/90' : 'focus:ring-sky-500'
-      }`}
-    />
-  )
+function computedClassName({ isAccent, hasError }: InputProps) {
+  let className = 'border-neutral-400 focus:ring-sky-500'
+
+  if (hasError) {
+    className = 'border-rose-500 focus:ring-rose-500/80'
+  } else if (isAccent) {
+    className = 'border-orange-600 focus:ring-orange-500/80'
+  }
+
+  return className
 }
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ isAccent = false, hasError = false, ...props }, ref) => {
+    const classes = computedClassName({ isAccent, hasError })
+
+    return (
+      <input
+        ref={ref}
+        {...props}
+        className={`w-full outline-none rounded-md px-5 py-2 text-gray-700 border focus:ring
+        ${classes}
+        ${props.className}
+        `}
+      />
+    )
+  }
+)
+
+Input.displayName = 'Input'
+
+export { Input }
