86cf309d29b6fcdfc020d240801957bcd98dda7f
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 01:42:26 -0300

    feat: also set the session storage when changing isLoggedIn value

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -78,6 +78,7 @@ export const auth = createSlice({
     },
     setIsLoggedIn: (state, action: PayloadAction<boolean>) => {
       state.isLoggedIn = action.payload
+      sessionStorage.setItem('isLoggedIn', String(action.payload))
     },
     setUser: (state, action: PayloadAction<AuthUser | null>) => {
       state.user = action.payload
