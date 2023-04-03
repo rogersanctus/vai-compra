35a1d904a8e4d8a04b701c5ace7c8bdfe3cab8eb
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 23:19:09 -0300

    feat: adds the content to the Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main className="bg-slate-100 h-full w-full">HOME</main>
+import { Categories } from './Categories'
+import { ProductsList } from './ProducstList'
+import { TopProducts } from './TopProducts'
+
+export default async function Home() {
+  return (
+    <main className="bg-slate-100 h-full w-full">
+      <Categories />
+      <TopProducts />
+      <ProductsList />
+    </main>
+  )
 }
