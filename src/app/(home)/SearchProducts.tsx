bc6700f82711a070094a6f1803cbc3d000b3af75
Rogério Santos <rogersanctus@gmail.com>
2023-04-03 02:48:26 -0300

    feat: moves the Search component from the home layout to a new component and implements the searching logic

---
+++ src/app/(home)/SearchProducts.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
+import { useAppDispatch } from '@/stores'
+import { searchProducts } from '@/stores/products'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { actions } from '@/stores/actions'
+
+const { productsAction } = actions
+
+export const SearchProducts = function () {
+  const [search, setSearch] = useState('')
+  const [mustHaveAllTerms, setMustHaveAllTerms] = useState(false)
+  const dispatch = useAppDispatch()
+
+  const router = useRouter()
+
+  async function onSearch() {
+    productsAction.setSearchingTerms(search)
+    await dispatch(searchProducts({ search, mustHaveAllTerms }))
+    router.push('/search')
+  }
+
+  useEffect(() => {
+    setSearch('')
+    setMustHaveAllTerms(false)
+  }, [])
+
+  return (
+    <div className="flex flex-col w-full mx-14">
+      <div className="flex">
+        <Input
+          name="serch-product"
+          placeholder="Produto ou descrição"
+          className="flex-grow w-full"
+          isAccent
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <Button
+          className="ml-2"
+          variant="secondary"
+          size="md"
+          onClick={onSearch}
+        >
+          Buscar
+        </Button>
+      </div>
+      <div className="flex items-center mt-4 gap-2">
+        <input
+          type="checkbox"
+          name="must-have-all-terms"
+          checked={mustHaveAllTerms}
+          className="bg-amber-500 text-amber-500 w-6 h-6 rounded cursor-pointer"
+          onChange={() => setMustHaveAllTerms(!mustHaveAllTerms)}
+        />
+        <label htmlFor="must-have-all-terms" className="text-white">
+          Incluir apenas resultados com todos os termos?
+        </label>
+      </div>
+    </div>
+  )
+} as unknown as () => JSX.Element
--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@/components/Button'
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
-import { Input } from '@/components/Input'
 import { ShoppingCartIcon } from '@heroicons/react/20/solid'
+import { SearchProducts } from './SearchProducts'
+import { Categories } from './Categories'
 
 export const metadata = {
   title: 'Vai! Compra - Home'
@@ -10,22 +10,6 @@ export const metadata = {
 
 interface HomeLayoutProps {
   children: React.ReactNode
-}
-
-function PrependHeader() {
-  return (
-    <div className="flex w-full mx-14">
-      <Input
-        name="serch-product"
-        placeholder="Produto ou descrição"
-        className="flex-grow w-full"
-        isAccent
-      />
-      <Button className="ml-2" variant="secondary" size="md">
-        Buscar
-      </Button>
-    </div>
-  )
 }
 
 function AppendHeader() {
@@ -44,8 +28,11 @@ function AppendHeader() {
 export default function HomeLayout({ children }: HomeLayoutProps) {
   return (
     <>
-      <Header prepend={<PrependHeader />} append={<AppendHeader />} />
-      <main>{children}</main>
+      <Header prepend={<SearchProducts />} append={<AppendHeader />} />
+      <main>
+        <Categories />
+        {children}
+      </main>
       <Footer />
     </>
   )
