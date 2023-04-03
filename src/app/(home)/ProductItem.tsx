790076531e5bb3783d75d49d8549f898a7b0eb75
Rogério Santos <rogersanctus@gmail.com>
2023-04-03 08:55:01 -0300

    feat: adds favourites interaction

--- src/app/(home)/ProductItem.tsx
+++ src/app/(home)/ProductItem.tsx
@@ -1,27 +1,93 @@
 'use client'
 
 import { Button } from '@/components/Button'
-import { Product } from '@/models/product'
+import { ProductWithFavourite } from '@/models/product'
+import { useAppSelector } from '@/stores'
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline'
+import { HeartIcon as HeartIconSolid } from '@heroicons/react/24/solid'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+
+import { actions } from '@/stores/actions'
 
 interface ProductProps {
-  product: Product
+  product: ProductWithFavourite
 }
+
+const { productsAction } = actions
+
 export function ProductItem({ product }: ProductProps) {
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn)
+  const [showFavourite, setShowFavourite] = useState(false)
+  const [canShowFavourite, setCanShowFavourite] = useState(false)
   const formatter = Intl.NumberFormat(undefined, {
     style: 'currency',
     currency: 'BRL'
   })
 
+  useEffect(() => {
+    setShowFavourite(false)
+    setCanShowFavourite(false)
+    //productsAction.resetProducts()
+  }, [])
+
+  useEffect(() => {
+    setCanShowFavourite(isLoggedIn)
+  }, [isLoggedIn])
+
+  function onHover() {
+    setShowFavourite(true)
+  }
+
+  function onLeave() {
+    if (product.is_favourite) {
+      return
+    }
+
+    setShowFavourite(false)
+  }
+
+  async function onFavouriteClick() {
+    try {
+      const is_favourite = !product.is_favourite
+      const response = await fetch('/api/users/favourites', {
+        body: JSON.stringify({
+          product_id: product.id,
+          is_favourite
+        }),
+        method: 'PUT'
+      })
+
+      if (!response.ok) {
+        throw new Error('Failed to favourite the product')
+      }
+
+      const data = await response.json()
+      productsAction.updateProduct(data.is_favourite)
+    } catch (error) {
+      console.error(error)
+      toast('Não foi possível favoritar o produto', {
+        type: 'error',
+        position: 'bottom-right'
+      })
+    }
+  }
+
   const price = formatter.format(product.price)
   return (
-    <li className="cursor-pointer flex flex-col rounded shadow-lg border border-gray-200 p-4 h-[430px] justify-stretch">
+    <li
+      className="cursor-pointer flex flex-col rounded shadow-lg border border-gray-200 p-4 h-[430px] justify-stretch relative"
+      onMouseEnter={onHover}
+      onMouseLeave={onLeave}
+    >
       <div className="flex-grow relative overflow-hidden">
         <Image
           src={product.image}
           alt={`${product.title} photo`}
           className="object-contain mix-blend-multiply"
           fill
+          sizes="800px"
         />
       </div>
       <div className="flex cursor-pointer h-12 overflow-hidden my-3">
@@ -42,6 +108,21 @@ export function ProductItem({ product }: ProductProps) {
           Comprar
         </Button>
       </div>
+      {canShowFavourite && (product.is_favourite || showFavourite) ? (
+        <div className="absolute w-8 h-8 z-10">
+          <button
+            className="w-full h-full "
+            onClick={onFavouriteClick}
+            title="Favoritar produto"
+          >
+            {product.is_favourite ? (
+              <HeartIconSolid className="text-rose-400" />
+            ) : (
+              <HeartIconOutline className="text-gray-400" />
+            )}
+          </button>
+        </div>
+      ) : null}
     </li>
   )
 }
