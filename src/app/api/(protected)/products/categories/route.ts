1d945b4b3b8be86799d1e45805834fbe4c78f88c
Rogério Santos <rogersanctus@gmail.com>
2023-04-02 23:15:36 -0300

    feat: creates the route to get the products categories

---
+++ src/app/api/(protected)/products/categories/route.ts
@@ -0,0 +1,24 @@
+import { fetchOnApi } from '@/app/api/externalApi'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  try {
+    const response = await fetchOnApi('/products/categories')
+
+    if (!response.ok) {
+      throw new Error('Could not fetch the product Categories from the api')
+    }
+
+    const categories = await response.json()
+
+    return NextResponse.json(categories)
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json(error, { status: 500 })
+    }
+
+    return new NextResponse('Unknown error: ' + JSON.stringify(error), {
+      status: 500
+    })
+  }
+}
