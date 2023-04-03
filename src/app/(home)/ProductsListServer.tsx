206e750cf4e8b9ab71ed5cf1daa5fdcd739971e2
Rogério Santos <rogersanctus@gmail.com>
2023-04-03 15:32:32 -0300

    feat: creates a kind of wrapper server component to fetch products and use the ProductList component, now client component, to render interactive products

---
+++ src/app/(home)/ProductsListServer.tsx
@@ -0,0 +1,56 @@
+import { Product, ProductWithFavourite } from '@/models/product'
+import { localFetch } from '../localApi'
+import { Favourite } from '@/models/favourite'
+import { productsMapper } from '@/lib/productsHelper'
+import { ProductsList } from './ProducstList'
+
+interface ProductListProps {
+  category?: string
+  products?: Product[]
+}
+
+async function fetchFavourites(): Promise<Favourite[]> {
+  const response = await localFetch('/api/users/favourites')
+
+  if (!response.ok) {
+    throw new Error('Could not get the favourites list')
+  }
+
+  return await response.json()
+}
+
+async function fetchProducts(): Promise<Product[]> {
+  const response = await localFetch('/api/products')
+
+  if (!response.ok) {
+    throw new Error('Could not get products list')
+  }
+
+  return await response.json()
+}
+
+export const ProductsListServer = async function ({
+  category,
+  products
+}: ProductListProps) {
+  let localProducts: ProductWithFavourite[] = []
+
+  if (products) {
+    localProducts = products
+  } else {
+    try {
+      const [favourites, products] = await Promise.all([
+        fetchFavourites(),
+        fetchProducts()
+      ])
+
+      if (Array.isArray(favourites) && Array.isArray(products)) {
+        localProducts = productsMapper(products, favourites)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return <ProductsList products={localProducts} category={category} />
+} as unknown as (props: ProductListProps) => JSX.Element
