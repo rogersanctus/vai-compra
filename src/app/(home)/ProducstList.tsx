d410054d98428b19ecaf9d6e9c58d2a4dd1ed46d
Rogério Santos <rogersanctus@gmail.com>
2023-04-03 04:19:19 -0300

    feat: makes the ProductsList component be reusable

--- src/app/(home)/ProducstList.tsx
+++ src/app/(home)/ProducstList.tsx
@@ -2,35 +2,47 @@ import { Product } from '@/models/product'
 import { localFetch } from '../localApi'
 import { ProductItem } from './ProductItem'
 
-export const ProductsList = async function () {
-  let products: Product[] = []
+interface ProductListProps {
+  category?: string
+  products?: Product[]
+}
 
-  try {
-    const response = await localFetch('/api/products')
+export const ProductsList = async function ({
+  category,
+  products
+}: ProductListProps) {
+  let localProducts: Product[] = []
 
-    if (!response.ok) {
-      throw new Error('Could not get products list')
+  if (products) {
+    localProducts = products
+  } else {
+    try {
+      const response = await localFetch('/api/products')
+
+      if (!response.ok) {
+        throw new Error('Could not get products list')
+      }
+
+      const data = await response.json()
+
+      if (Array.isArray(data)) {
+        localProducts = data
+      }
+    } catch (error) {
+      console.error(error)
     }
-
-    const data = await response.json()
-
-    if (Array.isArray(data)) {
-      products = data
-    }
-  } catch (error) {
-    console.error(error)
   }
 
   return (
     <div className="flex flex-col px-20 pb-20">
       <span className="text-left text-2xl uppercase my-4">
-        Todos os produtos
+        {category ? category : 'Todos os produtos'}
       </span>
       <ul className="grid grid-cols-4 gap-5">
-        {products.map((product) => (
+        {localProducts.map((product) => (
           <ProductItem product={product} key={product.id} />
         ))}
       </ul>
     </div>
   )
-} as unknown as () => JSX.Element
+} as unknown as (props: ProductListProps) => JSX.Element
