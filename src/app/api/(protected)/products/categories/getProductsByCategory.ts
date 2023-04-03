68da91bdb736d80f2858fed68d0c460f10338498
Rogério Santos <rogersanctus@gmail.com>
2023-04-03 04:21:24 -0300

    feat: creates a function to fetch products by their category

---
+++ src/app/api/(protected)/products/categories/getProductsByCategory.ts
@@ -0,0 +1,13 @@
+import { fetchOnApi } from '@/app/api/externalApi'
+import { Product } from '@/models/product'
+
+export async function getProductsByCategory(name: string) {
+  const response = await fetchOnApi('/products/category/' + encodeURI(name))
+
+  if (!response.ok) {
+    throw new Error('Could not fetch the products of category from the api')
+  }
+
+  const products = await response.json()
+  return products as Product[]
+}
