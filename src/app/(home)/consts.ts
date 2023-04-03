39c519c1d84462a57527e97bcbea4ca734afc9f9
Rogério Santos <rogersanctus@gmail.com>
2023-04-03 04:16:58 -0300

    feat: adds a translation map for Categories

---
+++ src/app/(home)/consts.ts
@@ -0,0 +1,7 @@
+export const CategoriesTranslated: Record<string, string> = {
+  all: 'Todas',
+  electronics: 'Eletrônicos',
+  jewelery: 'Joalheria',
+  "men's clothing": 'Roupas masculinas',
+  "women's clothing": 'Roupas femininas'
+}
