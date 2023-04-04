0b7fe63f86da6a6cab124241138956131d20d531
Rogério Santos <rogersanctus@gmail.com>
2023-04-04 00:48:07 -0300

    fix: error returning

--- src/app/api/(protected)/products/categories/route.ts
+++ src/app/api/(protected)/products/categories/route.ts
@@ -21,6 +21,7 @@ export async function GET(request: NextRequest) {
       })
     }
   }
+
   try {
     const response = await fetchOnApi('/products/categories')
 
@@ -28,16 +29,17 @@ export async function GET(request: NextRequest) {
       throw new Error('Could not fetch the product Categories from the api')
     }
 
-    const categories = (await response.json()) as string[]
+    const categories = await response.json()
 
     return NextResponse.json(['all', ...categories])
   } catch (error) {
+    console.error(error)
+
     if (error instanceof Error) {
       return NextResponse.json(error, { status: 500 })
     }
 
-    return new NextResponse('Unknown error: ' + JSON.stringify(error), {
-      status: 500
-    })
+    error = typeof error === 'object' ? error : { error: { message: error } }
+    return NextResponse.json({ error }, { status: 500 })
   }
 }
