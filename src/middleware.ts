8c3515ede4d0510deef8331d71709e4e71a67ed8
Rogério Santos <rogersanctus@gmail.com>
2023-04-06 15:57:55 -0300

    feat: adds the checkout page to the match list of the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -107,5 +107,11 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/user/:path*', '/login/:path*', '/signup/:path*', '/api/:path*']
+  matcher: [
+    '/user/:path*',
+    '/checkout/:path*',
+    '/login/:path*',
+    '/signup/:path*',
+    '/api/:path*'
+  ]
 }
