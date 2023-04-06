3e232e93ec1b54c52fc8b518a80b9ceeaedede11
Rogério Santos <rogersanctus@gmail.com>
2023-04-06 11:40:07 -0300

    feat: will not show the badge with products on cart count when there is none of them

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -15,9 +15,11 @@ export function ShoppingCart() {
 
   return (
     <div className="ml-10 flex flex-col items-center relative">
-      <div className="absolute flex items-center justify-center bg-gray-800 text-white text-xs rounded-full -top-3 -right-1 w-6 h-6 overflow-hidden">
-        <span>{itemsOnCart}</span>
-      </div>
+      {itemsOnCart === 0 ? null : (
+        <div className="absolute flex items-center justify-center bg-gray-800 text-white text-xs rounded-full -top-3 -right-1 w-6 h-6 overflow-hidden">
+          <span>{itemsOnCart}</span>
+        </div>
+      )}
       <a href="/cart" title="Carrinho">
         <ShoppingCartIcon className="h-8 w-8 text-purple-50" />
         <span className="text-purple-100 font-semibold text-xs">Carrinho</span>
